9e0c2f34880624a997c7e883cf4b5612c93eecc4
--global <subfusca@gmail.com>
2016-02-02 14:49:13 -0700

    Send stdout *and* stderr to /dev/null on 'which' conditionals

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,20 +1,24 @@
 function fish_greeting
-    if not which fortune > /dev/null
+    if not which fortune > /dev/null ^ /dev/null
         switch (uname)
         case Darwin
             echo Installing fortune and cowsay
             brew install fortune cowsay
         case Linux
             echo Installing fortune and cowsay
-            sudo apt-get install fortune cowsay
+            if which apt-get > /dev/null ^ /dev/null
+                sudo apt-get install fortune cowsay
+            else
+                sudo yum install fortune cowsay
+            end
         case '*'
             echo Wait ... where are we\? (uname), eh
         end
     end
     set -l toon bud-frogs
-    if which lolcat > /dev/null
+    if which lolcat > /dev/null ^ /dev/null
         fortune -s | cowsay -f $toon | lolcat
-    else if which fortune > /dev/null
+    else if which fortune > /dev/null ^ /dev/null
         fortune -s | cowsay -f $toon
     else
         echo Something fishy going on around here ...

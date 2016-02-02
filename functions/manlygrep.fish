ea8e8b35ef03a0f631b04aab4fd9a20a3c8a087b
--global <subfusca@gmail.com>
2016-02-02 11:02:27 -0700

    Updating manlygrep function to print usage info

--- functions/manlygrep.fish
+++ functions/manlygrep.fish
@@ -1,4 +1,9 @@
 function manlygrep -a dir name match -d 'recursive grep that does not bother matching in binary files'
-    find "$dir" -name "$name" -print | xargs grep -I "$match"
+    if test -n "$match"
+        find "$dir" -name "$name" -print | xargs grep -I "$match"
+    else
+        echo "Usage: manlygrep <directory-to-search> <filename-pattern> <grep-string-to-match>"
+        echo \n"e.g. manlygrep /my/files '*.txt' work"
+        echo \n"Don't forget to put single quotes around the file match expression to thwart the dreaded wildcard expansion demon"
+    end
 end
-# Pro tip: Put single quotes around the file match expression to thwart the dreaded wildcard expansion demon

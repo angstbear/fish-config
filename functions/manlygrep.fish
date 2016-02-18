c1922cec8a3bf927034fdda46f7da2635b950675
--global <subfusca@gmail.com>
2016-02-17 17:25:06 -0700

    Add line numbers to manlygrep function

--- functions/manlygrep.fish
+++ functions/manlygrep.fish
@@ -1,6 +1,6 @@
 function manlygrep -a dir name match -d 'recursive grep that does not bother matching in binary files'
     if test -n "$match"
-        find "$dir" -name "$name" -print | xargs grep -I "$match"
+        find "$dir" -name "$name" -print | xargs grep -nI "$match"
     else
         echo "Usage: manlygrep <directory-to-search> <filename-pattern> <grep-string-to-match>"
         echo \n"e.g. manlygrep /my/files '*.txt' work"

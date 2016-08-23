9204d8884905ec99cf9300ad03a0831aedfe3fc3
Kristopher <angstbear@users.noreply.github.com>
2016-08-23 11:56:54 -0600

    Update vars.fish

--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -6,7 +6,6 @@ set -U EDITOR $VISUAL
 switch (uname)
 case Darwin
     set __fish_prompt_hostname "mac-fnord"
-    set -x PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
 case Linux
 end
 

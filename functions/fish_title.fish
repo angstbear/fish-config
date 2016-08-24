4b46cd4e2b9350fe68e56dc8a3d3bb6e48767fb5
angstbear <subfusca@gmail.com>
2016-08-24 15:05:35 -0600

    Fix hostname for mac os

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -1,6 +1,8 @@
 function fish_title
     if hostname | grep 'alpha' > /dev/null
         echo "b-m-$USER@alpha $argv[1] "
+    else if uname | grep Darwin > /dev/null
+        echo "$USER@mac-fnord $argv[1] "
     else
         echo "$USER@"(hostname)" $argv[1] "
     end

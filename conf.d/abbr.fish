45fede5bdaca54743bc39fd969122806694cc2c0
angstbear <subfusca@gmail.com>
2019-03-27 14:03:19 -0600

    Fix abbreviations; add git-achievements

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -1,26 +1,27 @@
 # Abbreviations
 
-abbr -a v="vim"
-abbr -a vi="vim"
-abbr -a vmi="vim"
-abbr -a em="emacs"
-abbr -a e="emacs"
-abbr -a ..="cd .."
-abbr -a ...="cd ../.."
+abbr -a v vim
+abbr -a vi vim
+abbr -a vmi vim
+abbr -a em emacs
+abbr -a e emacs
+abbr -a .. "cd .."
+abbr -a ... "cd ../.."
 
-abbr -a cp="cp -iv"
-abbr -a mv="mv -iv"
-abbr -a rm="rm -iv"
+abbr -a cp "cp -iv"
+abbr -a mv "mv -iv"
+abbr -a rm "rm -iv"
 
-abbr -a gita="git add"
-abbr -a gitb="git branch"
-abbr -a gitc="git commit"
-abbr -a gitco="git checkout"
-abbr -a gitl="git log"
-abbr -a gits="git status"
-abbr -a ga="git add"
-abbr -a gb="git branch"
-abbr -a gc="git commit"
-abbr -a gco="git checkout"
-abbr -a gl="git log"
-abbr -a gs="git status"
+abbr -a git "git-achievements"
+abbr -a gita "git-achievements add"
+abbr -a gitb "git-achievements branch"
+abbr -a gitc "git-achievements commit"
+abbr -a gitco "git-achievements checkout"
+abbr -a gitl "git-achievements log"
+abbr -a gits "git-achievements status"
+abbr -a ga "git-achievements add"
+abbr -a gb "git-achievements branch"
+abbr -a gc "git-achievements commit"
+abbr -a gco "git-achievements checkout"
+abbr -a gl "git-achievements log"
+abbr -a gs "git-achievements status"
--- conf.d/vars.fish
+++ conf.d/vars.fish
@@ -3,6 +3,8 @@
 set -U VISUAL emacs
 set -U EDITOR $VISUAL
 
+set -x PATH ~/src/git-achievements $PATH
+
 switch (uname)
 case Darwin
     set __fish_prompt_hostname "mac-fnord"

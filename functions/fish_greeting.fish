7c33b86083c9ed90ab5e86c04e58659e92c66c7c
Kristopher Boren <kboren@proofpoint.com>
2024-03-29 17:40:44 -0600

    Update fish_greeting with darwin specs

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,4 +1,10 @@
 function fish_greeting
     fish_logo
+    if uname | grep Darwin > /dev/null
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        set specs '(macOS|boot|MacBook|Chip|32)'
+        system_profiler SPHardwareDataType SPSoftwareDataType  | egrep "$specs"
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+    end
     cat ~/.motd
 end
